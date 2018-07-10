998f1220fca6b6ad879467289566e4977bb21eee
Julin Maloof <jnmaloof@ucdavis.edu>
2018-07-10 16:10:40 -0700

    fix formatting in primes

--- Chapter3/100Primes.py
+++ Chapter3/100Primes.py
@@ -1,4 +1,4 @@
-#100 primes
+# 100 primes
 
 prime_total = 0
 
@@ -7,13 +7,13 @@ current_test = 1
 div_by = 0
 
 while (prime_total < 100):
-    start=min(current_test,2) #necessary so that we get "1"
-    for i in range(start,current_test+1):
+    start = min(current_test, 2)  # necessary so that we get "1"
+    for i in range(start, current_test+1):
         div_by = i
-        if (current_test % i == 0): #current_test divisible by i
+        if (current_test % i == 0):  # current_test divisible by i
             break
-    if (current_test == div_by): # this means we did not find current test to be divisible by any lower numbers
+    if (current_test == div_by):    # this means we did not find current test
+                                    # to be divisible by any lower numbers
         print(current_test)
         prime_total = prime_total + 1
     current_test = current_test + 1
-        
