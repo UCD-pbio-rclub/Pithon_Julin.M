c5db1a160d322dd2abc06adb95dd95d39eef0434
Julin Maloof <jnmaloof@ucdavis.edu>
2018-07-25 13:16:31 -0700

    FriendlyFastaCount ugly but working

--- Chapter7/FriendlyFastaCount.py
+++ Chapter7/FriendlyFastaCount.py
@@ -1,2 +1,31 @@
 from tkinter import *
-from countSeqFuns.py import countFasta
+import tkinter.filedialog as fd
+from Chapter6.countSeqFuns import countFasta
+
+
+class App:
+
+    def __init__(self, master):
+        self.file = StringVar()
+        self.count = IntVar()
+        frame = Frame(master)
+        frame.pack()
+        Button(frame,
+               text="Choose File",
+               command=self.get_file).grid(row=0, column=0)
+        Label(frame, textvariable=self.file).grid(row=0, column=1)
+        Button(frame, 
+               text="Count Sequences",
+               command=self.get_count).grid(row=1, column=0)
+        Label(frame, textvariable=self.count).grid(row=1, column=1)
+
+    def get_file(self):
+        self.file.set(fd.askopenfilename())
+
+    def get_count(self):
+        self.count.set(countFasta(self.file.get()))
+
+root = Tk()
+app = App(root)
+root.mainloop()
+
